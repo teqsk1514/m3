e635e0c64b8d84e01a3d52cd1ef97d01bee3476d
teqodoo <ravisk1514@gmail.com>
2018-09-30 22:49:16 +0530

    navbar and card done

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 var createError = require('http-errors');
 var express = require('express');
+var expressHbs = require('express-handlebars');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
 // view engine setup
+app.engine('.hbs', expressHbs({ defaultLayout: 'layout', extname: '.hbs' }))
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
@@ -23,12 +24,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
